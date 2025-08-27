373bb941c60a86d44b02ed40864e4c71596470cf
Edgar Jimenez <ebyted@gmail.com>
2025-08-27 14:15:38 -0700

    localhost

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3015;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:3015', 'https://valura.mx/api/cotizacion', 'https://www.valura.mx', 'http://valura-homepage'],
+  origin: ['http://localhost','http://localhost:3015', 'https://valura.mx/api/cotizacion', 'https://www.valura.mx', 'http://valura-homepage'],
   methods: ['POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
